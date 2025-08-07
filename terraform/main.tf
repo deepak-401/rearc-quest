fb129c2a7966273102b7dbed352c539b56e11e6b
Deepak Patra <145733842+deepak-401@users.noreply.github.com>
2025-08-07 14:09:39 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -73,6 +73,18 @@ resource "aws_instance" "rearc_quest_ec2" {
   subnet_id = aws_subnet.rearc_quest_public_subnet.id
   associate_public_ip_address = true
   vpc_security_group_ids = [ aws_security_group.rearc_quest_SG.id ]
+  user_data = <<-EOF
+             #!/bin/bash
+             yum update -y
+             yum install -y git
+             curl -sL https://rpm.nodesource.com/setup_18.x | bash -
+             yum install -y nodejs
+             cd /home/ec2-user
+             git clone https://github.com/deepak-401/rearc-quest.git
+             cd rearc-quest
+             npm install
+             node src/index.js > output.log 2>&1 &
+             EOF
   tags = {
     Name="rearc-quest-ec2"
   }
